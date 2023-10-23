844ffed084b6cf501d7e57486790d901bd58cfce
Ericdid <ericdidom@gmail.com>
2023-10-23 17:04:43 +0200

    fix.faker

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -20,12 +20,12 @@ class TrainsTableSeeder extends Seeder
         for ($i = 0; $i < 10; $i++) {
             $train = new Train();
 
-            $train->Azienda = $faker->company();
+            $train->Azienda = $faker->word();
             $train->Stazione_Di_Partenza = $faker->city();
             $train->Stazione_Di_Arrivo = $faker->city();
-            $train->orario_di_partenza = $faker->numberBetween(0, 24) . " : " . $faker->numberBetween(0, 59);
-            $train->orario_di_arrivo = $faker->numberBetween(0, 24) . " : " . $faker->numberBetween(0, 59);
-            $train->codice_treno = $faker->randomNumber(5, true);
+            $train->orario_di_partenza = $faker->time();
+            $train->orario_di_arrivo = $faker->time();
+            $train->codice_treno = $faker->randomNumber(4, true);
             $train->numero_carrozze = $faker->numberBetween(4, 9);
             $train->In_Orario = $faker->randomElement(['si', 'no']);
             $train->Cancellato = $faker->randomElement(['si', 'no']);
