0947ef1116636fc06e7595789fcd23e8255703b7
Ericdid <ericdidom@gmail.com>
2023-10-23 03:01:56 +0200

    add.faker

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\Train;
+
+use Faker\Generator as faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,20 +15,22 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(faker $faker)
     {
-        $train = new Train();
+        for ($i = 0; $i < 10; $i++) {
+            $train = new Train();
 
-        $train->Azienda = "Prova";
-        $train->Stazione_Di_Partenza = "Punto_A";
-        $train->Stazione_Di_Arrivo = "Punto_B";
-        $train->orario_di_partenza = 13.00;
-        $train->orario_di_arrivo = 14.00;
-        $train->codice_treno = 25423;
-        $train->numero_carrozze = 15;
-        $train->In_Orario = 'si';
-        $train->Cancellato = "no";
+            $train->Azienda = "Prova";
+            $train->Stazione_Di_Partenza = "Punto_A";
+            $train->Stazione_Di_Arrivo = "Punto_B";
+            $train->orario_di_partenza = 13.00;
+            $train->orario_di_arrivo = 14.00;
+            $train->codice_treno = 25423;
+            $train->numero_carrozze = 15;
+            $train->In_Orario = 'si';
+            $train->Cancellato = "no";
 
-        $train->save();
+            $train->save();
+        }
     }
 }
